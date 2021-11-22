37ad81b55a778666a97e30a83022a942a39b9267
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2021-11-21 21:44:22 -0500

    Add files via upload

---
+++ practicas/js/rebot.balls/main.js
@@ -0,0 +1,55 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+function Ball(x, y, velX, velY, color, size) {
+    this.x = x; //posición horizontal
+    this.y = y; //posición vertical
+    this.velX = velX; //velocidad horizontal
+    this.velY = velY; //velocidad vertical
+    this.color = color; //color
+    this.size = size; //tamaño
+  }
+
+  Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+var testBall = new Ball(50, 100, 4, 4, 'blue', 10);
+testBall.x
+testBall.size
+testBall.color
+testBall.draw()
+
+Ball.prototype.update = function() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
